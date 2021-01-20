d4fdd1c0e18d52c30f32d0d6819df1c9ffb68c22
root <root@ZenBookFLo.localdomain>
2021-01-20 23:26:31 +0100

    add cv component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoursComponent } from './cours/cours.component';
+import { CvComponent } from './cv/cv.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:'home'},
   {path: 'cours', component:CoursComponent},
   {path: 'home', component:HomeComponent},
+  {path: 'cv', component:CvComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CoursComponent } from './cours/cours.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CoursComponent
+    CoursComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
