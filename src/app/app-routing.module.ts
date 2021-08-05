d893dbe4a6bdf908473b38f4b1658b2c3f85ecb4
FlorentDht <43930524+FloFloDht@users.noreply.github.com>
2021-08-05 16:26:17 +0200

    suppression components pour un restart de l'app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,55 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { AnglaisComponent } from './anglais/anglais.component';
-import { ArchiJEEComponent } from './archi-jee/archi-jee.component';
-import { AROComponent } from './aro/aro.component';
-import { CommunicationComponent } from './communication/communication.component';
-import { ComplexiteComponent } from './complexite/complexite.component';
-import { CoursComponent } from './cours/cours.component';
-import { CryptoComponent } from './crypto/crypto.component';
 import { CvComponent } from './cv/cv.component';
-import { DPRComponent } from './dpr/dpr.component';
-import { ERDIComponent } from './erdi/erdi.component';
-import { FLComponent } from './fl/fl.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
-import { GLComponent } from './gl/gl.component';
 import { HomeComponent } from './home/home.component';
-import { IHMComponent } from './ihm/ihm.component';
-import { IntegrationDonneesComponent } from './integration-donnees/integration-donnees.component';
-import { ISDComponent } from './isd/isd.component';
-import { PhotosComponent } from './photos/photos.component';
-import { POCComponent } from './poc/poc.component';
-import { ProjetsComponent } from './projets/projets.component';
-import { ReseauxComponent } from './reseaux/reseaux.component';
-import { SecuAppliComponent } from './secu-appli/secu-appli.component';
-import { TerComponent } from './ter/ter.component';
+import {PhotosComponent} from './photos/photos.component';
+import {ProjetsComponent} from './projets/projets.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'cours', component: CoursComponent},
   {path: 'home', component: HomeComponent},
   {path: 'aboutme', component: AboutMeComponent},
   {path: 'cv', component: CvComponent},
   {path: 'projets', component: ProjetsComponent},
   {path : 'photos', component: PhotosComponent},
-  {path: 'POC', component: POCComponent},
-  {path: 'ARO', component: AROComponent},
-  {path: 'complexite', component: ComplexiteComponent},
-  {path: 'GL', component: GLComponent},
-  {path: 'ERDI', component: ERDIComponent},
-  {path: 'ISD', component: ISDComponent},
-  {path: 'reseaux', component: ReseauxComponent},
-  {path: 'anglais', component: AnglaisComponent},
-  {path: 'FL', component: FLComponent},
-  {path: 'DPR', component: DPRComponent},
-  {path: 'cryptographie', component: CryptoComponent},
-  {path: 'communication', component: CommunicationComponent},
-  {path: 'IHM', component: IHMComponent},
-  {path: 'securite_application', component: SecuAppliComponent},
-  {path: 'architecture_jee', component: ArchiJEEComponent},
-  {path: 'integration_donnees', component: IntegrationDonneesComponent},
-  {path: 'ter', component: TerComponent},
   {path: 'not-found', component: FourOhFourComponent },
   {path: '**', redirectTo: 'not-found' }
 
@@ -60,4 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routedComponent = [HomeComponent, CoursComponent];
+export const routedComponent = [HomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CoursComponent } from './cours/cours.component';
 import { CvComponent } from './cv/cv.component';
 import { ProjetsComponent } from './projets/projets.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
-import { POCComponent } from './poc/poc.component';
-import { AROComponent } from './aro/aro.component';
-import { ComplexiteComponent } from './complexite/complexite.component';
-import { GLComponent } from './gl/gl.component';
-import { ERDIComponent } from './erdi/erdi.component';
-import { ISDComponent } from './isd/isd.component';
-import { ReseauxComponent } from './reseaux/reseaux.component';
-import { AnglaisComponent } from './anglais/anglais.component';
-import { FLComponent } from './fl/fl.component';
-import { DPRComponent } from './dpr/dpr.component';
-import { IHMComponent } from './ihm/ihm.component';
-import { CryptoComponent } from './crypto/crypto.component';
-import { CommunicationComponent } from './communication/communication.component';
-import { SecuAppliComponent } from './secu-appli/secu-appli.component';
-import { ArchiJEEComponent } from './archi-jee/archi-jee.component';
-import { IntegrationDonneesComponent } from './integration-donnees/integration-donnees.component';
-import { TerComponent } from './ter/ter.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PhotosComponent } from './photos/photos.component';
 import { ConnexionComponent } from './connexion/connexion.component';
@@ -34,27 +16,9 @@ import { AdminComponent } from './admin/admin.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    CoursComponent,
     CvComponent,
     ProjetsComponent,
     FourOhFourComponent,
-    POCComponent,
-    AROComponent,
-    ComplexiteComponent,
-    GLComponent,
-    ERDIComponent,
-    ISDComponent,
-    ReseauxComponent,
-    AnglaisComponent,
-    FLComponent,
-    DPRComponent,
-    IHMComponent,
-    CryptoComponent,
-    CommunicationComponent,
-    SecuAppliComponent,
-    ArchiJEEComponent,
-    IntegrationDonneesComponent,
-    TerComponent,
     AboutMeComponent,
     PhotosComponent,
     ConnexionComponent,
